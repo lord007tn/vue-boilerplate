014af5afb5806caeae7413562c54fd6007e8b3e5
Raed Bahri <38087431+lord007tn@users.noreply.github.com>
2023-08-17 19:42:59 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useAppStore } from './stores/app'
 
+const appStore = useAppStore()
+const { hydrating } = storeToRefs(appStore)
 </script>
 
 <template>
-  <v-card></v-card>
-
-  <RouterView />
+  <div v-if="hydrating" class="fixed z-50">Loading</div>
+  <router-view v-else-if="!hydrating"></router-view>
 </template>
+
